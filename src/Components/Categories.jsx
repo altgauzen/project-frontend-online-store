e62407272a7d9d08ed247c6c0f0dfdb6950c7a6e
marcus <marcus.rodrigues10@hotmail.com>
2021-08-11 16:47:31 -0300

    finish requirement 06

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -6,12 +6,26 @@ export default class Categories extends Component {
     super(props);
     this.state = {
       categories: {},
+      productsFromCategory: [],
       loading: true,
     };
+    this.fetchCategories = this.fetchCategories.bind(this);
+    this.getCategoryId = this.getCategoryId.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategories();
+  }
+
+  async getCategoryId(event) {
+    const filterCategoryId = await
+    api.getProductsFromCategoryAndQuery(event.target.value, '');
+    //  console.log(event.target.value);
+    //  console.log(filterCategoryId.results);
+    this.setState({
+      loading: false,
+      productsFromCategory: filterCategoryId.results,
+    });
   }
 
   async fetchCategories() {
@@ -23,24 +37,36 @@ export default class Categories extends Component {
   }
 
   render() {
-    const { categories, loading } = this.state;
+    const { categories, loading, productsFromCategory } = this.state;
     if (loading) return <h4>Loading</h4>;
-    // console.log(this.state);
+    console.log(productsFromCategory);
+    console.log(this.props);
     return (
       <div>
-        {categories.map(({ id, name }) => (
-          <div key={ id }>
-            <label htmlFor={ id } data-testid="category">
-              <input
-                type="radio"
-                name="category"
-                id={ id }
-                value={ id }
-              />
-              { name }
-            </label>
-          </div>
-        ))}
+        <form>
+          {categories.map(({ id, name }) => (
+            <div key={ id }>
+              <label htmlFor={ id } data-testid="category">
+                <input
+                  type="radio"
+                  name="category"
+                  id={ id }
+                  value={ id }
+                  onClickCapture={ this.getCategoryId }
+                />
+                { name }
+              </label>
+            </div>
+          ))}
+        </form>
+        {productsFromCategory.length !== 0 ? productsFromCategory
+          .map(({ title, price, thumbnail, id }) => (
+            <div key={ id } data-testid="product">
+              <h3>{ title }</h3>
+              <p>{ price }</p>
+              <img src={ thumbnail } alt="foto" />
+            </div>
+          )) : ''}
       </div>
     );
   }
