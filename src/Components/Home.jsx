ef1e8ac64a651025466c35c3e53b4046399c5fb8
Genesis-Henriques <GenesisHenriques@gmail.com>
2021-08-12 19:09:00 -0300

    Requisito 9 pronto

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CartButton from './CartButton';
 import Categories from './Categories';
 import SearchBar from './SearchBar';
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cart: [],
-    };
-  }
-
-  addToCart = (product) => {
-    console.log('foi');
-    const {
-      id,
-      price,
-      title,
-      thumbnail,
-    } = product;
-    const newItem = { id, price, title, thumbnail };
-    this.setState((prevState) => ({
-      cart: [...prevState.cart, newItem],
-    }));
-  }
-
   render() {
-    const { cart } = this.state;
-    console.log(cart);
+    const { cart, addToCart } = this.props;
     return (
       <div>
-        <SearchBar addToCart={ this.addToCart } cart={ cart } />
-        <Categories addToCart={ this.addToCart } cart={ cart } />
+        <SearchBar addToCart={ addToCart } cart={ cart } />
+        <Categories addToCart={ addToCart } cart={ cart } />
         <CartButton cart={ cart } />
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  addToCart: PropTypes.func.isRequired,
+  cart: PropTypes.arrayOf(PropTypes.array).isRequired,
+};
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import CartButton from '../Components/CartButton';
 
 export default class ProductDetails extends Component {
   render() {
-    const { location } = this.props;
+    const { location, addToCart, cart } = this.props;
     const { title, thumbnail, price, attributes } = location.state;
     return (
       <div>
@@ -25,6 +26,8 @@ export default class ProductDetails extends Component {
         >
           Adicionar ao Carrinho
         </button>
+        <Link to="/">Voltar</Link>
+        <CartButton cart={ cart } />
       </div>
     );
   }
@@ -45,4 +48,6 @@ ProductDetails.propTypes = {
       ),
     }),
   }).isRequired,
+  addToCart: PropTypes.func.isRequired,
+  cart: PropTypes.arrayOf(PropTypes.array).isRequired,
 };
